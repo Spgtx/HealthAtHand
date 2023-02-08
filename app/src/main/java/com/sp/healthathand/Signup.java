382952681e46502a07f5b62ef35be62ea49c915b
Spgtx <78141711+Spgtx@users.noreply.github.com>
2023-02-08 08:40:09 -0800

    update

--- app/src/main/java/com/sp/healthathand/Signup.java
+++ app/src/main/java/com/sp/healthathand/Signup.java
@@ -1,14 +1,50 @@
 package com.sp.healthathand;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Signup extends AppCompatActivity {
+
+    TextInputLayout regName, regUsername, regEmail, regPhone_no, regPassword;
+    Button callLogin, signup;
+
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_signup);
+
+        callLogin = findViewById(R.id.bck_login);
+        signup = findViewById(R.id.signup);
+        callLogin.setOnClickListener(view -> {
+            Intent intent = new Intent(Signup.this, login.class);
+            startActivity(intent);
+        });
+
+        /*
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Signup.this, dashboard.class);
+                startActivity(intent);
+            }
+        });
+*/
+
     }
 }
